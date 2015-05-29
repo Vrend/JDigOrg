3263100f3a46606f08f0a187ea2511726f7d4818
s0urc3d3v3l0pm3nt <github@techredesigned.com>
2015-05-29 18:46:30 -0400

    Update addC.java

--- src/addC.java
+++ src/addC.java
@@ -1,3 +1,18 @@
+/**
+ *     This file is part of JDigOrg.
+
+    JDigOrg is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License
+
+    JDigOrg is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JDigOrg.  If not, see <http://www.gnu.org/licenses/>.
+    */
 import javax.swing.*;
 
 import java.awt.Container;
